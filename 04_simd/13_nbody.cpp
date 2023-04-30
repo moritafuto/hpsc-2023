b166c769f52ae5593aa5f583eb91deaa6cd4c088
moritafuto <130618781+moritafuto@users.noreply.github.com>
2023-05-01 00:42:06 +0900

    Update 13_nbody.cpp

--- 04_simd/13_nbody.cpp
+++ 04_simd/13_nbody.cpp
@@ -14,21 +14,24 @@ int main() {
   }
   for(int i=0; i<N; i++) {
     for(int j=0; j<N; j++) {
-    __m256 xivec = _mm256_load_ps(&(x[i]));
-    __m256 yivec = _mm256_load_ps(&(y[i]));
-    __m256 mivec = _mm256_load_ps(&(m[i]));
-    __m256 xjvec = _mm256_load_ps(&(x[j]));
-    __m256 yjvec = _mm256_load_ps(&(y[j]));
-    __m256 mjvec = _mm256_load_ps(&(m[j]));
-    __m256 mask = _mm256_cmpneq_ps(i, j, CMPNEQPS);
-    __m256 rxvec = _mm256_sub_ps(xivec, xjvec);
-    __m256 ryvec = _mm256_sub_ps(yivec, yjvec);
-    __m256 rvec = _mm256_rsqrt_ps(rxvec, ryvec);
- 
-        fx[i] -= rx * m[j] / (rvec * rvec * rvec);
-        fy[i] -= ry * m[j] / (rvec * rvec * rvec);
-      }
+       __m256 xivec = _mm256_load_ps(&(x[i]));
+       __m256 yivec = _mm256_load_ps(&(y[i]));
+       __m256 mivec = _mm256_load_ps(&(m[i]));
+       __m256 xjvec = _mm256_load_ps(&(x[j]));
+       __m256 yjvec = _mm256_load_ps(&(y[j]));
+       __m256 mjvec = _mm256_load_ps(&(m[j]));
+       __m256 fxvec = _mm256_load_ps(&(fx[i]));
+       __m256 fyvec = _mm256_load_ps(&(fy[i]));
+       __m256 mask = _mm_cmpneq_ps(i, j);
+       __m256 rxvec = _mm256_sub_ps(xivec, xjvec);
+       __m256 ryvec = _mm256_sub_ps(yivec, yjvec);
+       __m256 invrvec = _mm256_rsqrt_ps(rxvec*rxvec+ryvec*ryvec);
+       fxvec = _mm256_sub_ps(fxvec, rxvec * mjvec *invrvec * invrvec * invrvec);
+       fyvec = _mm256_sub_ps(fyvec, ryvec * mjvec *invrvec * invrvec * invrvec);
+       _mm256_store_ps(fx, fxvec);
+       _mm256_store_ps(fy, fyvec);
+       for(int i=0; i<N; i++)
+        printf("%d %g %g\n",i,fx[i],fy[i]);
     }
-    printf("%d %g %g\n",i,fx[i],fy[i]);
-  }
+   }
 }
