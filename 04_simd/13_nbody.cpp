21f8e9ac5f46b2ddd2850e23bd876c970283855c
moritafuto <130618781+moritafuto@users.noreply.github.com>
2023-05-30 21:34:36 +0900

    Update 13_nbody.cpp

--- 04_simd/13_nbody.cpp
+++ 04_simd/13_nbody.cpp
@@ -5,6 +5,8 @@
 
 int main() {
   const int N = 8;
+  const int vectorSize = 8;
+  const int iterations = N / vectorSize;
   float x[N], y[N], m[N], fx[N], fy[N];
   for(int i=0; i<N; i++) {
     x[i] = drand48();
@@ -12,26 +14,53 @@ int main() {
     m[i] = drand48();
     fx[i] = fy[i] = 0;
   }
-  for(int i=0; i<N; i++) {
-    for(int j=0; j<N; j++) {
-       __m256 xivec = _mm256_load_ps(&(x[i]));
-       __m256 yivec = _mm256_load_ps(&(y[i]));
-       __m256 mivec = _mm256_load_ps(&(m[i]));
-       __m256 xjvec = _mm256_load_ps(&(x[j]));
-       __m256 yjvec = _mm256_load_ps(&(y[j]));
-       __m256 mjvec = _mm256_load_ps(&(m[j]));
-       __m256 fxvec = _mm256_load_ps(&(fx[i]));
-       __m256 fyvec = _mm256_load_ps(&(fy[i]));
-       __m256 mask = _mm_cmpneq_ps(i, j);
-       __m256 rxvec = _mm256_sub_ps(xivec, xjvec);
-       __m256 ryvec = _mm256_sub_ps(yivec, yjvec);
-       __m256 invrvec = _mm256_rsqrt_ps(rxvec*rxvec+ryvec*ryvec);
-       fxvec = _mm256_sub_ps(fxvec, rxvec * mjvec *invrvec * invrvec * invrvec);
-       fyvec = _mm256_sub_ps(fyvec, ryvec * mjvec *invrvec * invrvec * invrvec);
-       _mm256_store_ps(fx, fxvec);
-       _mm256_store_ps(fy, fyvec);
-       for(int i=0; i<N; i++)
-        printf("%d %g %g\n",i,fx[i],fy[i]);
+  
+  __m256 rx_vec, ry_vec, r_vec, m_vec, rx2_vec, ry2_vec, r3_vec;
+  __m256 fx_vec, fy_vec;
+ 
+  for (int i = 0; i < iterations; i++) {
+    fx_vec = _mm256_setzero_ps();
+    fy_vec = _mm256_setzero_ps();
+
+    // Load x, y, and m vectors outside the loop
+    __m256 x_vec = _mm256_load_ps(&x[i * vectorSize]);
+    __m256 y_vec = _mm256_load_ps(&y[i * vectorSize]);
+    __m256 m_vec = _mm256_load_ps(&m[i * vectorSize]);
+
+    for (int j = 0; j < N; j += vectorSize) {
+      // Calculate the mask vector
+      int mask = (i == (j / vectorSize)) ? -1 : 0;
+
+      __m256i mask_vec = _mm256_set1_epi32(mask);
+
+      // Load x[j], y[j] vectors using the mask
+      __m256 xj_vec = _mm256_maskload_ps(&x[j], mask_vec);
+      __m256 yj_vec = _mm256_maskload_ps(&y[j], mask_vec);
+
+      // Calculate differences
+      rx_vec = _mm256_sub_ps(x_vec, xj_vec);
+      ry_vec = _mm256_sub_ps(y_vec, yj_vec);
+
+      rx2_vec = _mm256_mul_ps(rx_vec, rx_vec);
+      ry2_vec = _mm256_mul_ps(ry_vec, ry_vec);
+
+      r_vec = _mm256_rsqrt_ps(_mm256_add_ps(rx2_vec, ry2_vec));
+      r3_vec = _mm256_mul_ps(r_vec, _mm256_mul_ps(r_vec, r_vec));
+
+      // Multiply rx, ry with m[j] and divide by r^3 using the mask
+      __m256 rxm_vec = _mm256_and_ps(m_vec, _mm256_castsi256_ps(mask_vec)) * rx_vec;
+      __m256 r3inv_vec = _mm256_div_ps(_mm256_set1_ps(1.0f), r3_vec);
+      fx_vec -= _mm256_and_ps(m_vec, _mm256_castsi256_ps(mask_vec)) * (rxm_vec * r3inv_vec);
+      fy_vec -= _mm256_and_ps(_mm256_mul_ps(ry_vec, _mm256_and_ps(m_vec, _mm256_castsi256_ps(mask_vec))), r3inv_vec);
     }
-   }
-}
+
+    _mm256_store_ps(&fx[i * vectorSize], fx_vec);
+    _mm256_store_ps(&fy[i * vectorSize], fy_vec);
+  }
+
+  for (int i = 0; i < N; i++) {
+    printf("%d %g %g\n", i, fx[i], fy[i]);
+  }
+
+  return 0;
+}   
